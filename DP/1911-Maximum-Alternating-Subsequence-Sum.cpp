795a980f62a578dfa54a0f4d55160a5263bb820e
karun217 <karunpoddar0@gmail.com>
2025-07-12 21:16:09 +0530

    Add C++ solution for Leetcode 1911- Maximum alternating subsequence sum

--- DP/1911-Maximum-Alternating-Subsequence-Sum.cpp
+++ DP/1911-Maximum-Alternating-Subsequence-Sum.cpp
@@ -1,31 +1,31 @@
-// /*
-//  * LeetCode Problem 1911: Maximum Alternating Subsequence Sum
-//  * Link     : https://leetcode.com/problems/maximum-alternating-subsequence-sum
-//  * Tags     : Dynamic Programming, Greedy, Array
-//  * Companies: Quince
-//  * 
-//  * Time Complexity  : O(N), where N is the number of elements in nums
-//  * Space Complexity : O(N), for the DP table
-//  */
+/*
+ * LeetCode Problem 1911: Maximum Alternating Subsequence Sum
+ * Link     : https://leetcode.com/problems/maximum-alternating-subsequence-sum
+ * Tags     : Dynamic Programming, Greedy, Array
+ * Companies: Quince
+ * 
+ * Time Complexity  : O(N), where N is the number of elements in nums
+ * Space Complexity : O(N), for the DP table
+ */
 
-//  #define ll long long
-// class Solution {
-// public:
-//     ll solve(int i, vector<int>&nums , bool flag , vector<vector<ll>>& dp){
-//         //flag == true means even
-//         int n=nums.size();
-//         if(i>=n)return 0;
-//         if(dp[i][flag]!=-1) return dp[i][flag];
+ #define ll long long
+class Solution {
+public:
+    ll solve(int i, vector<int>&nums , bool flag , vector<vector<ll>>& dp){
+        //flag == true means even
+        int n=nums.size();
+        if(i>=n)return 0;
+        if(dp[i][flag]!=-1) return dp[i][flag];
 
-//         ll take = solve(i+1 , nums , !flag ,dp) + ((flag == true)? nums[i] : -nums[i]);
-//         ll notake = solve(i+1 , nums , flag ,dp);
-//         return dp[i][flag]= max(take , notake);
-//     }
+        ll take = solve(i+1 , nums , !flag ,dp) + ((flag == true)? nums[i] : -nums[i]);
+        ll notake = solve(i+1 , nums , flag ,dp);
+        return dp[i][flag]= max(take , notake);
+    }
 
-//     long long maxAlternatingSum(vector<int>& nums){
-//         int n=nums.size();
-//         vector<vector<ll>>dp(n+1 , vector<ll>(2,-1));
-//         return solve(0 ,nums, true ,dp);
-//     }
-// };
+    long long maxAlternatingSum(vector<int>& nums){
+        int n=nums.size();
+        vector<vector<ll>>dp(n+1 , vector<ll>(2,-1));
+        return solve(0 ,nums, true ,dp);
+    }
+};
 
